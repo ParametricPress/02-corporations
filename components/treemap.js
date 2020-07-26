5e2ef603bece878060391757c5f95a2ad2935807
Geoffrey Litt <gklitt@gmail.com>
2020-07-26 17:11:32 -0400

    initial attempt at a national treemap

--- components/treemap.js
+++ components/treemap.js
@@ -7,13 +7,19 @@ const Color = require("color");
 const width = 800;
 const height = 600;
 
-const treemap = (data) =>
-  d3
+const treemap = (data) => {
+  const hierarchicalData = d3
+    .hierarchy(data)
+    .sum((d) => d.data.value)
+    .sort((a, b) => b.data.value - a.data.value);
+
+  return d3
     .treemap()
-    .tile(d3.treemapBinary)
+    .tile(d3.treemapSquarify)
     .size([width, height])
     .padding(1)
-    .round(true)(d3.hierarchy(data));
+    .round(true)(hierarchicalData);
+};
 
 class Treemap extends React.Component {
   constructor(props) {
@@ -44,15 +50,29 @@ class Treemap extends React.Component {
         return d.parent;
       })(groupedCountries);
 
+    // todo: don't run on render, precompute?
+    const treemapData = treemap(root);
+
     console.log("rendering");
-    console.log("data", data);
-    console.log("root", root);
-    console.log("treemap", treemap(data));
+    console.log("treemap", treemapData);
 
     return (
       <div {...props}>
         <svg width={width} height={height}>
-          <text y={100}>Hello world</text>
+          {treemapData.leaves().map((d) => {
+            return (
+              <g key={d.data.id} transform={`translate(${d.x0},${d.y0})`}>
+                <text>{d.data.id}</text>
+                <rect
+                  width={d.x1 - d.x0}
+                  height={d.y1 - d.y0}
+                  fill="red"
+                  opacity={0.5}
+                  stroke="black"
+                />
+              </g>
+            );
+          })}
         </svg>
       </div>
     );
